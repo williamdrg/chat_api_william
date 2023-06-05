5c5f72d38d3d2cfeee85832bb376509596ec882a
unknown <davidwdr@ggmail.com>
2023-06-04 23:22:06 -0500

    actualización 1

--- src/app.js
+++ src/app.js
@@ -10,14 +10,6 @@ initModel()
 const app = express()
 app.use(express.json())
 
-db.sync()
-  .then(() => console.log('Base de datos sincronizada'))
-  .catch( err => console.error(err))
-
-app.get('/', (req, res) => {
-  res.send('servidor funcionando todo ok')
-})
-
 apiRoutes(app)
 errorRoutes(app)
 
